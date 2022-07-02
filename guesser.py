d0e2d3d8161c75c9509e354c25ffa5d6796656ac
LaceBowr <97077997+LaceBowr@users.noreply.github.com>
2022-07-02 11:18:40 -0700

    Since we are past due on these i took the liberty of finishing this

---
+++ guesser.py
@@ -0,0 +1,23 @@
+from objects.objects import Paper, Rock, Scissors
+class Display():
+    def __init__(self) -> None:
+        pass
+    def print_guess(self, guess):
+        if isinstance(guess, Scissors):
+            print("0   0")
+            print("  X ")
+            print(" / \\")
+            print("/   \\")
+
+        elif isinstance(guess, Rock):
+            print("  ____")
+            print("/      \\")
+            print("\ ____ /")
+
+        elif isinstance(guess, Paper):
+            print("   _______")
+            print("  /      /,")   
+            print(" /      //")
+            print("/______//")
+        else:
+            print("Unknown Guess Type: You did something wrong")
