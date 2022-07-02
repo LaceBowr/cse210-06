10b5d1ccd9a12dcebc0b5db026f7152dc183b2d9
LaceBowr <97077997+LaceBowr@users.noreply.github.com>
2022-07-02 00:22:53 -0700

    We needed to combine the the paper,py, rock.py, and scissors.py classes in the same file.  sorry.

---
+++ objects.py
@@ -0,0 +1,32 @@
+from objects.guess import Guess
+
+
+class Paper(Guess):
+    def __init__(self):
+        #the object known as paper that the guesser and computer are choosing in the game
+        #Paper always beats the Rock in an instance
+        pass
+    def beats(self, guess):
+        if isinstance(guess, Rock):
+            return True
+        return False
+
+class Scissors(Guess):
+    def __init__(self):
+        #the object known as Sissors that the guesser and computer are choosing in the game
+        #Scissors always beats the Paper in an instance
+        pass
+    def beats(self, guess):
+        if isinstance(guess, Paper):
+            return True
+        return False
+
+class Rock(Guess):
+    def __init__(self):
+        #the object known as Rock that the guesser and computer are choosing in the game
+        #Rock always beats the Scissors in an instance
+        pass
+    def beats(self, guess):
+        if isinstance(guess, Scissors):
+            return True
+        return False
