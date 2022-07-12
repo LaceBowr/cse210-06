b3aa4dee392305181ba3e7946d37158998f813ec
Jordan <48775765+JordanTheDailyWire@users.noreply.github.com>
2022-07-11 21:35:14 -0700

    Update guesser.py
    I have commented on the guesser.py file, explaining what the code does in the file.

--- guesser.py
+++ guesser.py
@@ -1,10 +1,16 @@
+# Import Rock, Paper, and Scissors
 from objects import Paper,Rock,Scissors
 
+# Create class guesser
 class Guesser():
 
+    # Create the init function
     def __init__(self):
         pass
 
+    # Create the get guess and do a while
+    # statement that allows the user guess
+    # to choose Rock, Paper, Scissors
     def get_guess(self):
         while True:
             text = input("Do you guess (P)aper, (R)ock, or (S)cissors: ")
@@ -17,6 +23,11 @@ class Guesser():
                 return Scissors()
             else:
                 print("Invalid input, try again")
+                
+# This code shows the design of the objects in the game
+# It prints out the Rock, Paper, and Siccors after the
+# user has guess, it will then display any error
+# messages if the user has type in the wrong letter
 '''from objects import Paper, Rock, Scissors
 class Display():
     def __init__(self) -> None:
